007ceab28e8904a3a4b8559b543963f56c4f7ed5
Artyom Tretyakov <arty0spr@gmail.com>
2025-10-02 09:17:45 +0500

    Add User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  balance: {
+    type: Number,
+    default: process.env.INITIAL_BALANCE || 10000,
+    min: 0
+  },
+  portfolio: [{
+    symbol: {
+      type: String,
+      required: true,
+      uppercase: true
+    },
+    name: String,
+    amount: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    averagePrice: {
+      type: Number,
+      required: true,
+      min: 0
+    }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Method to get user without password
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+export default mongoose.model('User', userSchema);
